ebe4dbe0d77affc909943924695b86216ed7d9f7
Dave Lowe <107778859+davefl67@users.noreply.github.com>
2024-05-06 09:51:49 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "eof-version"
+description "It's that thing that does a thing. I forked it from that guy I saw in that place that time. This resource is to be included in my releases to check for updates."
+author "Blumlaut (forked by davefl67)"
+
+server_scripts {
+	'versioncheck.lua'
+}
